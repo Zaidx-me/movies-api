55a0c1ba2cb512842c93feda6cb7d61c4057ba4d
Zaidx-me <m.zaid.tech@gmail.com>
2026-07-29 07:07:09 +0500

    fix: remove i18n plugin, verify API domain

--- streamlab/app/plugins/i18n.client.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-
-export default defineNuxtPlugin(() => {
-  const i18n = useI18n()
-  if (process.client) {
-    const storedLocale = localStorage.getItem('streamlab-locale')
-    if (storedLocale) i18n.setLocale(storedLocale)
-  }
-})
