ce9ecc94b595b365c2656369cda9fe7b95648652
Zaidx-me <m.zaid.tech@gmail.com>
2026-07-29 06:57:22 +0500

    fix: update MovieBox API domain to movies.158.101.18.206.sslip.io

--- streamlab/server/utils/api.ts
+++ streamlab/server/utils/api.ts
@@ -1,11 +1,11 @@
-const BASE = 'http://movies-api.158.101.18.206.sslip.io'
+const BASE = 'http://movies.158.101.18.206.sslip.io'
 
 export async function fetchMovieBox<T>(endpoint: string): Promise<T> {
   const res = await fetch(`${BASE}${endpoint}`, {
     headers: {
       'User-Agent': 'StreamLab/1.0',
       'Accept': 'application/json',
-      'Referer': 'http://movies-api.158.101.18.206.sslip.io',
+      'Referer': 'http://movies.158.101.18.206.sslip.io',
     },
   })
   if (!res.ok) throw createError({ statusCode: res.status, message: `MovieBox API error ${res.status}` })
